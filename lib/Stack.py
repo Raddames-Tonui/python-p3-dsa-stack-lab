363e2bae76dd8ea4284849212e5dd524c30d4aca
Raddames <raddamestonui48@gmail.com>
2024-06-07 23:44:34 +0300

    completed

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,19 +1,19 @@
 class Stack:
-
-    def __init__(self, items = [], limit = 100):
+    def __init__(self, items=None, limit=100):
+        if items is None:
+            items = []
         self.items = []
         self.limit = limit
 
         for item in items:
-            if(not self.full()):
+            if not self.full():
                 self.items.append(item)
-
 
     def isEmpty(self):
         return self.items == []
 
     def push(self, item):
-        if (not self.full()):
+        if not self.full():
             self.items.append(item)
         else:
             return None
@@ -24,19 +24,22 @@ class Stack:
         return self.items.pop()
 
     def peek(self):
-        return self.items[len(self.items)]
+        if self.isEmpty():
+            return None
+        return self.items[-1]
 
     def size(self):
         return len(self.items)
 
     def full(self):
-
-        if (len(self.items) >= self.limit):
-            return True
-        return False
+        return len(self.items) >= self.limit
 
     def search(self, target):
         for i in reversed(range(len(self.items))):
             if self.items[i] == target:
-                return len(self.items) -1  - i 
+                return len(self.items) - 1 - i
         return -1
+
+# Example usage
+my_stack = Stack([3, 3, 4, 2, 4, 5, 4, 2, 4, 6, 87, 4, 2, 5, 7, 2])
+print(my_stack.peek())
